b2217497de855f870fbb45f5e2ca1a520ecc4e86
URA7COB <udhayakumar.raajan@in.bosch.com>
2020-12-03 13:31:00 +0530

    Redux test changes

--- src/redux-test.js
+++ src/redux-test.js
@@ -25,9 +25,12 @@ const rootReducer = (state = initialState, action) => {
 const store = redux.createStore(rootReducer);
 console.log(store.getState());
 
+// Subscribe
+store.subscribe(() => {
+    console.log('[Subscription]: ', store.getState());
+})
+
 // Dispatch
 store.dispatch({ type: 'INCREMENT_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 5 });
 console.log(store.getState());
-
-// Subscribe
